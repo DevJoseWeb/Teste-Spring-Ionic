72da8e8cabcf549c8175841da75e2188d07a0ad1
Felippe Deiró <sergiofelippe.deiro@hotmail.com>
2018-05-30 07:55:18 -0300

    Criando repository e service para Categoria

--- cursomc/src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ cursomc/src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -1,30 +1,30 @@
 package com.nelioalves.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.services.CategoriaService;
 
 //classe é um controlador rest
 @RestController
-//vai responder pelo endpoint abaixo. padrao rest é no plural
-@RequestMapping(value="/categorias")
+// vai responder pelo endpoint abaixo. padrao rest é no plural
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
-//	tem q usar os verbos http p cada operacao. obtendo um dado
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
-		
-		List<Categoria> lista = new ArrayList<>();
-		lista.addAll(Arrays.asList(cat1, cat2));
-		
-		return lista;
+	@Autowired
+	private CategoriaService service;
+
+	// tem q usar os verbos http p cada operacao. obtendo um dado
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	// @PathVariable, p saber q id da url tem q ir p parametro do metodo
+	// retorno, ja encapsula com varias informacoes de resposta http p um serviço rest. <?> pode encontrar ou n
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
---
+++ cursomc/src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.nelioalves.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	// a dependencia é automaticamente instanciada pelo spring atraves da injecao de dependencias ou inversao de controle
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
