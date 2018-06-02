11faacd4b79b8ac2f2d6d5166bfb6fc910e49219
Felippe Deiró <sergiofelippe.deiro@hotmail.com>
2018-06-02 11:42:49 -0300

    Paginação com parametros opcionais na requisição

--- cursomc/src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ cursomc/src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -5,11 +5,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -29,48 +31,66 @@ public class CategoriaResource {
 	// tem q usar os verbos http p cada operacao. obtendo um dado
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	// @PathVariable, p saber q id da url tem q ir p parametro do metodo
-	// retorno, ja encapsula com varias informacoes de resposta http p um serviço rest. <?> pode encontrar ou n
+	// retorno, ja encapsula com varias informacoes de resposta http p um serviço
+	// rest. <?> pode encontrar ou n
 	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
-		//n é bom um metodo no controlador rest ser grande com try e catch
-		/*quando der erro no categoria service, classe objectnotfound é acionada, a classe 
-		 * resourceexception tbm é acionada, q recebe o request e a mensagem do error, preenche a 
-		 * classe standarderror com as informacoes e retorna em formato json*/
+		// n é bom um metodo no controlador rest ser grande com try e catch
+		/*
+		 * quando der erro no categoria service, classe objectnotfound é acionada, a
+		 * classe resourceexception tbm é acionada, q recebe o request e a mensagem do
+		 * error, preenche a classe standarderror com as informacoes e retorna em
+		 * formato json
+		 */
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	
-	@RequestMapping(method=RequestMethod.POST)
-	//resposta http(responseentity) e n vai ter corpo(void)
-	//p obj ser construido atraves dos dados json q enviar, coloca a anotacao, faz o json ser convertido p objeto
+
+	@RequestMapping(method = RequestMethod.POST)
+	// resposta http(responseentity) e n vai ter corpo(void)
+	// p obj ser construido atraves dos dados json q enviar, coloca a anotacao, faz
+	// o json ser convertido p objeto
 	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
-		//quando insere, o banco de dados atribui um novo id ao obj
+		// quando insere, o banco de dados atribui um novo id ao obj
 		obj = service.insert(obj);
-		//pega a id e fornece como argumento p a uri
-		/* envia a nova uri como resposta a requisicao q cria com sucesso uma nova categoria
-		 * localhost:8080/categorias/3
-		 * */
+		// pega a id e fornece como argumento p a uri
+		/*
+		 * envia a nova uri como resposta a requisicao q cria com sucesso uma nova
+		 * categoria localhost:8080/categorias/3
+		 */
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
-		//gera o codigo 201
+		// gera o codigo 201
 		return ResponseEntity.created(uri).build();
 	}
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Categoria> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
-	
+
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<CategoriaDTO>> findAll() {
 		List<Categoria> list = service.findAll();
 		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+	}
+
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	// parametro opcional. http://localhost:8080/categorias/page?linesPerPage=3&page=0
+	// /page?linesPerPage=3&page=2&direction=DESC
+	public ResponseEntity<Page<CategoriaDTO>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+		Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<CategoriaDTO> listDto = list.map(obj -> new CategoriaDTO(obj));
 		return ResponseEntity.ok().body(listDto);
 	}
 }
